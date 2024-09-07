eaeef45cdd9b42e3d2d063828f7a812b89c3d1f5
oksana-rybalka <rybalka-oksana@outlook.com>
2024-09-07 13:07:49 +0300

    fix-bug-task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
-    newArray = [];
-    for(let i = 0; i <= numbers.length; i++) {
+    const newArray = [];
+    for(let i = 0; i <= numbers.length-1; i++) {
        if (numbers[i] > value) {
         newArray.push(numbers[i]);
        }
